2d6aba93eb2cbec21dee996d79cbed3a846cb798
yangs2012 <yangs2012@qq.com>
2017-10-04 15:52:11 +0800

    add 今日复习

--- app/src/main/java/com/yangs/medicine/fragment/ErrorTodayFragment.java
+++ app/src/main/java/com/yangs/medicine/fragment/ErrorTodayFragment.java
@@ -1,30 +1,43 @@
 package com.yangs.medicine.fragment;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.yangs.medicine.R;
+import com.yangs.medicine.activity.APPlication;
 import com.yangs.medicine.adapter.ErrorTodayAdapter;
 import com.yangs.medicine.model.ErrorTodayList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yangs on 2017/10/3 0003.
  */
 
-public class ErrorTodayFragment extends LazyLoadFragment implements ErrorTodayAdapter.OnItemClickListener {
+public class ErrorTodayFragment extends LazyLoadFragment implements ErrorTodayAdapter.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
     private View mLay;
     private RecyclerView mrecyclerView;
     private List<ErrorTodayList> list;
-    private List<ErrorTodayList> tmpList;
+    private Map<String, List<ErrorTodayList>> map;
     private ErrorTodayAdapter errorTodayAdapter;
+    private SwipeRefreshLayout srl;
+    private Handler mHandler;
 
     @Override
     protected int setContentView() {
@@ -35,21 +48,46 @@ public class ErrorTodayFragment extends LazyLoadFragment implements ErrorTodayAd
     protected void lazyLoad() {
         if (isInit) {
             if (!isLoad) {
-                initData();
+                list = new ArrayList<>();
+                map = new HashMap<>();
+                initHandler();
                 mLay = getContentView();
-                mrecyclerView = (RecyclerView) mLay.findViewById(R.id.errortoday_frag_rv);
+                srl = (SwipeRefreshLayout) mLay.findViewById(R.id.errortoday_srl);
+                srl.setColorSchemeColors(Color.RED, Color.GREEN, ContextCompat.getColor(getContext(),
+                        R.color.colorPrimary));
+                mrecyclerView = (RecyclerView) mLay.findViewById(R.id.errortoday_rv);
                 errorTodayAdapter = new ErrorTodayAdapter(list, getActivity());
-                errorTodayAdapter.setOnItemClickListener(this);
+                errorTodayAdapter.setOnItemClickListener(ErrorTodayFragment.this);
                 mrecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
                 mrecyclerView.setAdapter(errorTodayAdapter);
                 mrecyclerView.addItemDecoration(new DividerItemDecoration(
                         getActivity(), DividerItemDecoration.VERTICAL));
+                srl.setOnRefreshListener(this);
+                srl.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        srl.setRefreshing(true);
+                        onRefresh();
+                    }
+                });
             }
         }
     }
 
+    private void initHandler() {
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case 1:
+                        break;
+                }
+            }
+        };
+    }
+
     private void initData() {
-        list = new ArrayList<>();
         ErrorTodayList errorTodayList = new ErrorTodayList();
         errorTodayList.setName("生理学");
         errorTodayList.setClick(false);
@@ -76,20 +114,33 @@ public class ErrorTodayFragment extends LazyLoadFragment implements ErrorTodayAd
             return;
         if (list.get(position).getClick()) {
             list.get(position).setClick(false);
-            list.removeAll(tmpList);
+            list.removeAll(map.get(list.get(position).getName()));
         } else {
             list.get(position).setClick(true);
-            tmpList = new ArrayList<>();
+            List<ErrorTodayList> tmpList = new ArrayList<>();
             ErrorTodayList errorTodayList = new ErrorTodayList();
-            errorTodayList.setName("休克时的补液原则是()");
+            errorTodayList.setName(position + " 休克时的补液原则是()");
             errorTodayList.setType("small");
             tmpList.add(errorTodayList);
             errorTodayList = new ErrorTodayList();
             errorTodayList.setName("DNA双螺旋结构的维持主要靠核苷酸间的磷酸");
             errorTodayList.setType("small");
             tmpList.add(errorTodayList);
-            list.addAll(position + 1, tmpList);
+            map.put(list.get(position).getName(), tmpList);
+            list.addAll(position + 1, map.get(list.get(position).getName()));
         }
         errorTodayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onRefresh() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                srl.setRefreshing(false);
+                initData();
+                errorTodayAdapter.notifyDataSetChanged();
+            }
+        }, 2000);
     }
 }
