47d996c83945388ab3dacfe21cc3125c45fdcdf3
yangs2012 <yangs2012@qq.com>
2017-10-07 23:52:12 +0800

    状态栏变色背景

--- app/src/main/java/com/yangs/medicine/activity/MainActivity.java
+++ app/src/main/java/com/yangs/medicine/activity/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -51,6 +53,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void init() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
         fm = getSupportFragmentManager();
         tab_ly_1 = (LinearLayout) findViewById(R.id.tab_ly_1);
         tab_ly_2 = (LinearLayout) findViewById(R.id.tab_ly_2);
