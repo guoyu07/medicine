8514794b03310e4fb8bc206e2c08258975f1d3b9
yangs2012 <yangs2012@qq.com>
2017-10-10 14:29:33 +0800

    add 选择题

--- app/src/main/java/com/yangs/medicine/activity/QuestionActivity.java
+++ app/src/main/java/com/yangs/medicine/activity/QuestionActivity.java
@@ -7,10 +7,14 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.yangs.medicine.R;
 import com.yangs.medicine.adapter.TitleBuilder;
 import com.yangs.medicine.fragment.QuestionFragment;
+import com.yangs.medicine.question.ChooseQuesFragment;
 import com.yangs.medicine.util.FitStatusBar;
 
 import java.util.ArrayList;
@@ -20,36 +24,27 @@ import java.util.List;
  * Created by yangs on 2017/9/24 0024.
  */
 
-public class QuestionActivity extends BaseActivity {
-    private TitleBuilder titleBuilder;
+public class QuestionActivity extends BaseActivity implements View.OnClickListener {
+    private Button bt_back;
+    private Button bt_ok;
+    private LinearLayout ll_chat;
+    private ImageView iv_love;
+    private ImageView iv_share;
+    private ImageView iv_chat;
+    private ImageView iv_timu;
     private ViewPager viewPager;
     private List<Fragment> list;
+    private ChooseQuesFragment chooseQuesFragment1;
+    private ChooseQuesFragment chooseQuesFragment2;
+    private ChooseQuesFragment chooseQuesFragment3;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questionactivity_layout);
         FitStatusBar.addStatusBarView(this);
-        list = new ArrayList<>();
-        QuestionFragment answerFragment = new QuestionFragment();
-        list.add(answerFragment);
-        answerFragment = new QuestionFragment();
-        list.add(answerFragment);
-        answerFragment = new QuestionFragment();
-        list.add(answerFragment);
-        viewPager = (ViewPager) findViewById(R.id.question_ac_vp);
-        titleBuilder = new TitleBuilder(this);
-        titleBuilder.setLeftOnclickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        }).setTitleText("临床医学").setRightBtText("交卷").setRightBtOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                APPlication.showToast("交卷...", 0);
-            }
-        });
+        initView();
+        initData();
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
@@ -78,9 +73,69 @@ public class QuestionActivity extends BaseActivity {
         });
     }
 
+    private void initData() {
+        list = new ArrayList<>();
+        chooseQuesFragment1 = new ChooseQuesFragment();
+        list.add(chooseQuesFragment1);
+        chooseQuesFragment2 = new ChooseQuesFragment();
+        list.add(chooseQuesFragment2);
+        chooseQuesFragment3 = new ChooseQuesFragment();
+        list.add(chooseQuesFragment3);
+    }
+
+    private void initView() {
+        bt_back = (Button) findViewById(R.id.questionactivity_head_back);
+        bt_ok = (Button) findViewById(R.id.questionactivity_head_ok);
+        ll_chat = (LinearLayout) findViewById(R.id.questionactivity_ll);
+        iv_love = (ImageView) findViewById(R.id.questionactivity_iv_love);
+        iv_share = (ImageView) findViewById(R.id.questionactivity_iv_share);
+        iv_chat = (ImageView) findViewById(R.id.questionactivity_iv_talk);
+        iv_timu = (ImageView) findViewById(R.id.questionactivity_iv_timu);
+        viewPager = (ViewPager) findViewById(R.id.questionactivity_vp);
+        bt_back.setOnClickListener(this);
+        bt_ok.setOnClickListener(this);
+        ll_chat.setOnClickListener(this);
+        iv_love.setOnClickListener(this);
+        iv_share.setOnClickListener(this);
+        iv_chat.setOnClickListener(this);
+        iv_timu.setOnClickListener(this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.questionactivity_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.questionactivity_head_back:
+                finish();
+                break;
+            case R.id.questionactivity_head_ok:
+                if (viewPager.getCurrentItem() == 0)
+                    chooseQuesFragment1.checkOK();
+                if (viewPager.getCurrentItem() == 1)
+                    chooseQuesFragment2.checkOK();
+                if (viewPager.getCurrentItem() == 2)
+                    chooseQuesFragment3.checkOK();
+                break;
+            case R.id.questionactivity_ll:
+                APPlication.showToast("评论", 0);
+                break;
+            case R.id.questionactivity_iv_love:
+                APPlication.showToast("收藏", 0);
+                break;
+            case R.id.questionactivity_iv_share:
+                APPlication.showToast("分享", 0);
+                break;
+            case R.id.questionactivity_iv_talk:
+                APPlication.showToast("讨论", 0);
+                break;
+            case R.id.questionactivity_iv_timu:
+                APPlication.showToast("题目", 0);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/yangs/medicine/question/ChooseQuesFragment.java
@@ -0,0 +1,191 @@
+package com.yangs.medicine.question;
+
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.yangs.medicine.R;
+import com.yangs.medicine.fragment.LazyLoadFragment;
+
+/**
+ * Created by yangs on 2017/10/10 0010.
+ * 选择题Fragment
+ * 复杂界面,后期模块化
+ */
+
+public class ChooseQuesFragment extends LazyLoadFragment implements View.OnClickListener {
+    private View mLay;
+    //问题
+    private TextView tv_ques;
+    //A选项
+    private LinearLayout ll_A;
+    private TextView ll_A_1;
+    private LinearLayout ll_A_2;
+    private LinearLayout ll_A_3;
+    private TextView ll_A_4;
+    //B选项
+    private LinearLayout ll_B;
+    private TextView ll_B_1;
+    private LinearLayout ll_B_2;
+    private LinearLayout ll_B_3;
+    private TextView ll_B_4;
+    //C选项
+    private LinearLayout ll_C;
+    private TextView ll_C_1;
+    private LinearLayout ll_C_2;
+    private LinearLayout ll_C_3;
+    private TextView ll_C_4;
+    //D选项
+    private LinearLayout ll_D;
+    private TextView ll_D_1;
+    private LinearLayout ll_D_2;
+    private LinearLayout ll_D_3;
+    private TextView ll_D_4;
+    //E选项
+    private LinearLayout ll_E;
+    private TextView ll_E_1;
+    private LinearLayout ll_E_2;
+    private LinearLayout ll_E_3;
+    private TextView ll_E_4;
+
+    @Override
+    protected int setContentView() {
+        return R.layout.choosequesfrgament;
+    }
+
+    @Override
+    protected void lazyLoad() {
+        if (isInit) {
+            if (!isLoad) {
+                initView();
+            }
+        }
+
+    }
+
+    private void initView() {
+        mLay = getContentView();
+        tv_ques = (TextView) mLay.findViewById(R.id.choosequesfrag_tv_ques);
+        ll_A = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_A);
+        ll_B = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_B);
+        ll_C = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_C);
+        ll_D = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_D);
+        ll_E = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_E);
+        ll_A_1 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_A_1);
+        ll_B_1 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_B_1);
+        ll_C_1 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_C_1);
+        ll_D_1 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_D_1);
+        ll_E_1 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_E_1);
+        ll_A_2 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_A_2);
+        ll_B_2 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_B_2);
+        ll_C_2 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_C_2);
+        ll_D_2 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_D_2);
+        ll_E_2 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_E_2);
+        ll_A_3 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_A_3);
+        ll_B_3 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_B_3);
+        ll_C_3 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_C_3);
+        ll_D_3 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_D_3);
+        ll_E_3 = (LinearLayout) mLay.findViewById(R.id.choosequesfrag_ll_E_3);
+        ll_A_4 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_A_4);
+        ll_B_4 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_B_4);
+        ll_C_4 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_C_4);
+        ll_D_4 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_D_4);
+        ll_E_4 = (TextView) mLay.findViewById(R.id.choosequesfrag_ll_E_4);
+        ll_A.setOnClickListener(this);
+        ll_B.setOnClickListener(this);
+        ll_C.setOnClickListener(this);
+        ll_D.setOnClickListener(this);
+        ll_E.setOnClickListener(this);
+    }
+
+    private int b = 0;
+    private int d = 0;
+    private int a = 0;
+    private int c = 0;
+    private int e = 0;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.choosequesfrag_ll_A:
+                reset();
+                ll_A_1.setBackgroundResource(R.drawable.topic_tab_lay6);
+                ll_A_1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                a = 1;
+                break;
+            case R.id.choosequesfrag_ll_B:
+                reset();
+                ll_B_1.setBackgroundResource(R.drawable.topic_tab_lay6);
+                ll_B_1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                b = 1;
+                break;
+            case R.id.choosequesfrag_ll_C:
+                reset();
+                ll_C_1.setBackgroundResource(R.drawable.topic_tab_lay6);
+                ll_C_1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                c = 1;
+                break;
+            case R.id.choosequesfrag_ll_D:
+                reset();
+                ll_D_1.setBackgroundResource(R.drawable.topic_tab_lay6);
+                ll_D_1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                d = 1;
+                break;
+            case R.id.choosequesfrag_ll_E:
+                reset();
+                ll_E_1.setBackgroundResource(R.drawable.topic_tab_lay6);
+                ll_E_1.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+                e = 1;
+                break;
+            case R.id.questionactivity_head_ok:
+                checkOK();
+                break;
+
+        }
+    }
+
+    private void reset() {
+        ll_A_1.setBackgroundResource(R.drawable.topic_tab_lay3);
+        ll_A_1.setTextColor(ContextCompat.getColor(getContext(), R.color.error_tv));
+        ll_B_1.setBackgroundResource(R.drawable.topic_tab_lay3);
+        ll_B_1.setTextColor(ContextCompat.getColor(getContext(), R.color.error_tv));
+        ll_C_1.setBackgroundResource(R.drawable.topic_tab_lay3);
+        ll_C_1.setTextColor(ContextCompat.getColor(getContext(), R.color.error_tv));
+        ll_D_1.setBackgroundResource(R.drawable.topic_tab_lay3);
+        ll_D_1.setTextColor(ContextCompat.getColor(getContext(), R.color.error_tv));
+        ll_E_1.setBackgroundResource(R.drawable.topic_tab_lay3);
+        ll_E_1.setTextColor(ContextCompat.getColor(getContext(), R.color.error_tv));
+        b = 0;
+        d = 0;
+        a = 0;
+        c = 0;
+        e = 0;
+    }
+
+    public void checkOK() {
+        ll_B_1.setVisibility(View.GONE);
+        ll_B_2.setVisibility(View.VISIBLE);
+        ll_B_3.setVisibility(View.GONE);
+        if (a == 1) {
+            ll_A_1.setVisibility(View.GONE);
+            ll_A_2.setVisibility(View.GONE);
+            ll_A_3.setVisibility(View.VISIBLE);
+        }
+        if (e == 1) {
+            ll_E_1.setVisibility(View.GONE);
+            ll_E_2.setVisibility(View.GONE);
+            ll_E_3.setVisibility(View.VISIBLE);
+        }
+        if (c == 1) {
+            ll_C_1.setVisibility(View.GONE);
+            ll_C_2.setVisibility(View.GONE);
+            ll_C_3.setVisibility(View.VISIBLE);
+        }
+        if (d == 1) {
+            ll_D_1.setVisibility(View.GONE);
+            ll_D_2.setVisibility(View.GONE);
+            ll_D_3.setVisibility(View.VISIBLE);
+        }
+    }
+}
